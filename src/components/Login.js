d18b32fcb1eff119fb9e6b210c971cfc7db9ec7d
BEKKAR SOFIANE <bekkarsofiane00@gmail.com>
2025-01-23 20:52:52 +0100

    first commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { loginSuccess } from '/react/mini-projet-react/src/store/actions'; 
+import { loginSuccess } from '../store/actions'; 
 import { verifyLogin } from '../api/LoginAPI'; 
 import { useNavigate } from 'react-router-dom';
 import './CSS/Login.css';
